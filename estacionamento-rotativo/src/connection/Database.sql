27fd11b8a033dbcb3f4adaa8899a766a6fb2d146
Artur Falcão <128255473+ArturFalD@users.noreply.github.com>
2023-10-19 21:44:46 -0300

    Semana 2 - CRUD Adicionar Motorista

--- estacionamento-rotativo/src/connection/Database.sql
+++ estacionamento-rotativo/src/connection/Database.sql
@@ -8,3 +8,15 @@ CREATE TABLE vaga (
     obliqua boolean NOT NULL,
     PRIMARY KEY (idVaga)
 );
+
+CREATE TABLE `motorista` (
+    idMotorista int NOT NULL AUTO_INCREMENT,
+    nomeCompleto varchar(150) NOT NULL,
+    genero varchar(15) NOT NULL,
+    rg varchar(50) NOT NULL,
+    cpf varchar(50) NOT NULL,
+    celular varchar(50) NOT NULL,
+    email varchar(150) NOT NULL,
+    senha varchar(150) NOT NULL,
+    PRIMARY KEY (idMotorista)
+);
